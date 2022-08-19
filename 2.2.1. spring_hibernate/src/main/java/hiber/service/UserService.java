976101fc6cd221d7e14c4ab17d85eb9f8692f874
Polesos1 <gaverdov.pavel@gmail.com>
2022-08-19 14:32:28 +0300

    Выполнил

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -1,10 +1,22 @@
 package hiber.service;
 
+import hiber.model.Car;
 import hiber.model.User;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface UserService {
     void add(User user);
+
+    @Transactional
+    void add(Car car);
+
     List<User> listUsers();
+
+    @Transactional(readOnly = true)
+    List<Car> listCars();
+
+    User getUserByCar(Car car);
 }
+
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -1,6 +1,9 @@
 package hiber.service;
 
 import hiber.dao.UserDao;
+
+
+import hiber.model.Car;
 import hiber.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,19 +14,40 @@ import java.util.List;
 @Service
 public class UserServiceImp implements UserService {
 
-   @Autowired
-   private UserDao userDao;
+    @Autowired
+    private UserDao userDao;
 
-   @Transactional
-   @Override
-   public void add(User user) {
-      userDao.add(user);
-   }
+    public UserServiceImp(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
-   @Transactional(readOnly = true)
-   @Override
-   public List<User> listUsers() {
-      return userDao.listUsers();
-   }
+    @Transactional
+    @Override
+    public void add(User user) {
+        userDao.add(user);
+    }
 
+    @Transactional
+    @Override
+    public void add(Car car) {
+        userDao.add(car);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<User> listUsers() {
+        return userDao.listUsers();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Car> listCars() {
+        return userDao.listCars();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public User getUserByCar(Car car) {
+        return userDao.getUserByCar(car);
+    }
 }
